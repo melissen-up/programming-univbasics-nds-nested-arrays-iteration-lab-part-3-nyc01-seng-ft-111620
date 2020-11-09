a170308555ebf1efbbdfad2f0c251c011748c05a
Melissa F <melissa.faulner@gmail.com>
2020-11-09 16:36:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,10 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
         combined_string << src[row_index][element_index]
-      element_index += 1
       end
-    row_index += 1
+      element_index += 1
     end
+    row_index += 1
   end
   combined_string.join(" ")
   combined_string
